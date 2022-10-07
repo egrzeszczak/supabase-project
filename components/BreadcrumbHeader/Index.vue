54eee46e68441f369259debdf5d825c690ed7c4d
egrzeszczak <ernest.grzeszczak@protonmail.com>
2022-10-07 18:29:42 +0200

    docs: :memo: Breadcrumb component description updated

--- components/BreadcrumbHeader/Index.vue
+++ components/BreadcrumbHeader/Index.vue
@@ -1,3 +1,31 @@
+<!-- 
+Breadcrumb with H1 component
+
+USE:    Example below
+
+<BreadcrumbHeader 
+    :breadcrumbs="
+            [ 
+                {   // INFO: First item will always be Home pointing to "/"
+                    name: 'Services',
+                    link: '/services',
+                },
+                {
+                    name: 'Requests',
+                    link: '/services/requests',
+                },
+                {   // INFO: Last item in 'breadcrumbs' array will be a h1 header displayed below the breadcrumbs (it will not be a breadcrumb)
+                    name: 'Create request',
+                    link: '/services/requests/create',
+                },
+            ]" 
+        />
+
+TODO:   Add recursive function to concat all paths from previous breadcrumb items
+        Right now you have to insert it like: '/a', '/a/b', '/a/b/c' - but I want '/a', '/b', '/c' in the future
+
+TODO:   Add icon support https://daisyui.com/components/breadcrumbs/
+-->
 <script setup>
 const props = defineProps(['breadcrumbs'])
 </script>
@@ -6,12 +34,14 @@ const props = defineProps(['breadcrumbs'])
     <div class="text-sm breadcrumbs">
         <ul>
             <li>
+                <!-- INFO: First item will always be Home pointing to "/" -->
                 <nuxt-link to="/">Home</nuxt-link>
             </li>
-            <li v-for="step, index in props.breadcrumbs" :key="step.name" >
+            <li v-for="step, index in props.breadcrumbs" :key="step.name">
                 <nuxt-link v-if="index != props.breadcrumbs.length - 1" :to="step.link">{{step.name}}</nuxt-link>
             </li>
         </ul>
     </div>
+    <!-- INFO: Last item in 'breadcrumbs' array will be a h1 header displayed below the breadcrumbs (it will not be a breadcrumb) -->
     <h1 class="text-2xl">{{props.breadcrumbs[props.breadcrumbs.length-1].name}}</h1>
 </template>
